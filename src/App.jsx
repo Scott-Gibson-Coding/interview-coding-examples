af2054dd9af7e0ee09c7cf536567067e14446baf
Scott-Gibson-Coding <scottchgibson@gmail.com>
2024-07-22 08:50:23 -0700

    Initial commit with some coding scenarios

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {
+  StateExample1,
+  StateExample2,
+  StateExample3,
+  PureFunction1,
+} from "./components/StateExamples";
+
+function App() {
+  return (
+    <>
+      <h1>State Example 1</h1>
+      <StateExample1 />
+      <h2>State Example 2</h2>
+      <StateExample2 />
+      <h2>State Example 3</h2>
+      <StateExample3 />
+      <h2>Pure Function 1</h2>
+      <PureFunction1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StateExamples.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+
+export function StateExample1() {
+  const [count, setCount] = useState(0);
+
+  function handleIncCountBy2() {
+    setCount(count + 1);
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <p>
+        Current count is: <span>{count}</span>
+      </p>
+      <button onClick={handleIncCountBy2}>Inc by 2</button>
+    </div>
+  );
+}
+
+export function StateExample2() {
+  const [count, setCount] = useState(0);
+
+  function incCountAndUseNewValue() {
+    setCount(count + 1);
+    handleNewCount(count);
+  }
+
+  function handleNewCount(val) {
+    console.log(val);
+  }
+
+  return (
+    <div>
+      <p>
+        Current count is: <span>{count}</span>
+      </p>
+      <button onClick={incCountAndUseNewValue}>Inc and use</button>
+    </div>
+  );
+}
+
+export function StateExample3() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  function updatePositionX() {
+    setPosition({ x: 45 });
+  }
+
+  return (
+    <div>
+      <p>
+        Position: [{position.x}, {position.y}]
+      </p>
+      <button onClick={updatePositionX}>Update</button>
+    </div>
+  );
+}
+
+const mockEntries = ["a", "b", "c"];
+async function getEntries() {
+  return new Promise((resolve) => resolve(mockEntries));
+}
+export function PureFunction1() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    async function populateEntries() {
+      const apiEntries = await getEntries(); // ["a", "b", "c"]
+
+      setEntries((entries) => {
+        entries = entries.concat(apiEntries);
+        return entries;
+      });
+    }
+    populateEntries();
+  }, []);
+
+  return (
+    <div>
+      <ul style={{ padding: 0 }}>
+        {entries.map((entry, index) => (
+          <li
+            style={{
+              display: "inline",
+              background: "#3300CCCC",
+              textIndent: "none",
+              paddingBlock: "2px",
+              paddingInline: "4px",
+              marginRight: "8px",
+              fontSize: "1.25rem",
+              color: "white",
+            }}
+            key={index}
+          >
+            {entry}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
